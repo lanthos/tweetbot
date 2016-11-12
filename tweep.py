5d9a4fd07de2fd5bd5c08be36e46302c44e47dc4
Jeremy Kenyon <lanthos@gmail.com>
2016-11-12 08:58:11 -0800

    Updated to fully loop through the results instead of just show a single one.

--- tweep.py
+++ tweep.py
@@ -10,23 +10,27 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+keyword = "trump"
+
 
 class MyListener(StreamListener):
     def on_data(self, data):
         try:
-            with open('pokemon.json', 'a') as f:
+            with open('racist.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
-                print "Error on_data: {}".format(e)
+                print("Error on_data: {}".format(e))
         return True
-    def on_error(self, status):
-        print status
-        return True
-twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=["#pokemongo"])
 
-with open('pokemon.json', 'r') as f:
-    line = f.readline()
-    tweet = json.loads(line)
-    print json.dumps(tweet, indent=4)
+    def on_error(self, status):
+        print(status)
+        return True
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=[keyword])
+
+with open('racist.json', 'r') as f:
+    for line in f:
+        tweet = json.loads(line)
+        print(json.dumps(tweet, indent=4))
